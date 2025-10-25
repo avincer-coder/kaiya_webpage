4ece00dccc33909bdf50dd87fd46c24441b451f8
Mario Eduardo Garcia Salazar <marioeduardo.garciasalazar@gmail.com>
2025-10-25 15:35:10 -0600

    Miner update

--- scripts/main.js
+++ scripts/main.js
@@ -12,7 +12,7 @@ function updateText() {
     textElementTwo.textContent = "developmental milestones, or the unique needs of a neurodiverse child, my service provides real-time, personalized guidance from the comfort of your home."
   } else {
   textElement.textContent = "Welcome to MamaSupport, your trusted partner in navigating the beautiful yet challenging journey of parenthood. My mission is to support young parents. MamaSupport offers a secure, on-demand video chat platform that connects you with someone to answer your questions, concerns, struggles, and anything else. Whether you’re dealing with sleepless nights,";
-  textElementTwo.textContent = "Welcome to MamaSupport, your trusted partner in navigating the beautiful yet challenging journey of parenthood. My mission is to support young parents. MamaSupport offers a secure, on-demand video chat platform that connects you with someone to answer your questions, concerns, struggles, and anything else. Whether you’re dealing with sleepless nights,";
+  textElementTwo.textContent = "Welcome to MamaSupport, your trusted partner in navigating the beautiful yet challenging journey of parenthood. My mission is to support young parents, MamaSupport offers a secure, on-demand video chat platform that connects you with someone to answer your questions, concerns, struggles, and anything else. Whether you’re dealing with sleepless nights, developmental milestones, or the unique needs of a neurodiverse child, my service provides real-time, personalized guidance from the comfort of your home.";
 }
 
 }
